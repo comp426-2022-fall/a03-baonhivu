56fb0338153d194b90f0702fedc9eadf53f7d147
Bao-Nhi Vu <69761376+baonhivu@users.noreply.github.com>
2022-11-16 22:13:21 -0500

    Added in side, dice, and rolls as a const

--- bin/cli.js
+++ bin/cli.js
@@ -4,4 +4,9 @@ import { roll } from "/lib/roll.js";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
+
+const side = args.side || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
+
 console.log(roll(argv["side"], argv["dice"], argv{"rolls"]));
