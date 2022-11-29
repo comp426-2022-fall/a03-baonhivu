8973a5ce8c73abcf0b5be9ad592f6b07445729b0
Bao-Nhi Vu <69761376+baonhivu@users.noreply.github.com>
2022-11-29 10:54:53 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { roll } from "/lib/roll.js";
+import roll from "../lib/roll.js";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 
-const side = args.side || 6;
+const sides = args.sides || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
 
-console.log(roll(argv["side"], argv["dice"], argv{"rolls"]));
+console.log(roll(args.sides, args.dice, args.rolls));
